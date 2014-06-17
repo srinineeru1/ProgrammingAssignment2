b54515c0c22b97630d681c194395cd1bd6dcda2d
srinineeru1 <srinineeru@outlook.com>
2014-06-17 11:07:53 -0700

    Final version after cleaning up.

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,30 @@
-## makeCacheMatrix function caches a matrix and its inverse to enable 
+## makeCacheMatrix function caches a matrix and its inverse to enable
 ## easy retrieval of inverse of the matrix without recomputing
 ## cacheSolve function computes inverse of a matrix if not found in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
     ## Caches a matrix and its inverse
     ## Place the input matrix into cache
-    setMat <- function(y) {
+#    if (!exists("i")) i <- NULL
+    i <- NULL
+    set <- function(y) {
         x <<- y
         i <<- NULL
     }
     # Return the matrix from the cache
-    getMat <- function() {
-        x
-    }
+    get <- function() x
     # Place the inverse of matrix into cache
-    setInv <- function(invert) {
-        i <<- invert
-    }
+    setinverse <- function(invert) i <<- invert
     # Return inverse of matrix from cache
-    getInv <- function() {
-        i
-    }
-    clearMat <- function() {
+    getinverse <- function() i
+    clearCache <- function() {
         x <<- NULL
         i <<- NULL
     }
-
-    list(setMat = setMat, getMat = getMat,
-         setInv = setInv,
-         getInv = getInv,
-         clearMat = clearMat)
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse,
+         clearCache = clearCache)
 }
 
 
@@ -38,16 +33,14 @@ cacheSolve <- function(x, ...) {
     ## Check if inverse of the matrix is in cache
     ## if inverse is a matrix with all elements as NA, it is not in cache
     ## then compute the inverse and place it in the cache for future calls
-    inv = makeCacheMatrix(x)$getInv()
+    inv = x$getinverse()
     if(!is.null(inv)) {
-        print("getting cached data")
+        print("getting inverse from cache")
         return(inv)
     }
     else {
-#        inv <- solve(x, ...)
-        makeCacheMatrix(x)$setInv(solve(x, ...))
-#        m <- makeCacheMatrix(x)$getMat()
-#        print(m)
-        makeCacheMatrix(x)$getInv()
+        inv <- solve(x$get())
+        x$setinverse(inv)
+        inv
     }
 }
